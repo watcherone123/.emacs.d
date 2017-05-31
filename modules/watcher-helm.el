20ba270a21b5c5baf03475e6484a8bcf00d379f8
watcher <watcherone123@gmail.com>
2017-05-31 21:30:22 +0800

    repair helm startup slowly bug

--- modules/watcher-helm.el
+++ modules/watcher-helm.el
@@ -1,10 +1,13 @@
 (use-package helm-ag
   :defer t
   :ensure t
+  :init
+  (setq tramp-ssh-controlmaster-options
+	"-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
   :config
   (custom-set-variables
    '(helm-ag-base-command "pt -e --nocolor --nogroup"))
-  
+
   )
 
-(provide 'watcher-helm) 
+(provide 'watcher-helm)
