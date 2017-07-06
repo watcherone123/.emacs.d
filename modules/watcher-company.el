48fd2c6e1c71b0f069d2b539082c603dcace0f59
watcherone123 <watcherone123@gmail.com>
2017-07-06 22:15:42 +0800

    add hippie-expand to company module

--- modules/watcher-company.el
+++ modules/watcher-company.el
@@ -11,8 +11,8 @@
 
 
   ;;this make it easy to get out of a nested expansion
-  (define-key yas-minor-mode-map (kbd "M-s-/") 'yas-next-field)
-  (global-set-key (kbd "M-/") 'yas-expand-from-trigger-key)
+  ;; (define-key yas-minor-mode-map (kbd "M-s-/") 'yas-next-field)
+  ;; (global-set-key (kbd "M-/") 'yas-expand-from-trigger-key)
   (defun watcher/load-yasnippt ()
     (unless yas-global-mode (yas-global-mode 1))
     (yas-minor-mode)
@@ -50,10 +50,11 @@
 
 (use-package company
   :ensure t
-  :defer t
-  :commands (global-company-mode)
+  ;; :defer t
+  ;; :commands (company-complete)
   :init
   (global-company-mode)
+  :config
   (setq company-dabbrev-downcase nil
 	;; make previous/next selection in the popup cycles
 	company-selection-wrap-around t
@@ -65,19 +66,44 @@
 	company-idle-delay 0.2
 	company-require-match nil
 	)
-  :config
-  (add-to-list 'company-backends
-	       '(company-capf company-dabbrev company-files
-			      company-dabbrev-code company-gtags
-			      company-keywords
-			      ))
-  ;; (setq company-backends
-  ;;	'((company-capf company-dabbrev company-files)
-  ;;	  (company-dabbrev-code company-gtags company-etags
-  ;;				company-keywords)))
+
+  ;; (add-to-list 'company-backends
+  ;;	       '(company-capf company-dabbrev company-files
+  ;;			      company-dabbrev-code company-gtags
+  ;;			      company-keywords
+  ;;			      ))
+  (setq company-backends
+	'((company-capf company-dabbrev company-files)
+	  (company-dabbrev-code company-gtags company-etags
+				company-keywords)))
+
+ (setq company-transformers
+	'(company-sort-by-occurrence))
+
+  (setq company-frontends
+	'(company-pseudo-tooltip-frontend
+	  company-echo-metadata-frontend))
+
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p")'company-select-previous)
+  (global-set-key (kbd "M-1") 'company-files)
+  ;; (global-set-key (kbd "M-/") 'company-complete)
+  )
 
+(use-package hippie-exp
+  :config
+  (setq hippie-expand-try-function-list '(try-expand-debbrev
+					  try-expand-debbrev-all-buffers
+					  try-expand-debbrev-from-kill
+					  try-complete-file-name-partially
+					  try-complete-file-name
+					  try-expand-all-abbrevs
+					  try-expand-list
+					  try-expand-line
+					  try-complete-lisp-symbol-partially
+					  try-complete-lisp-symbol))
+  (global-set-key (kbd "M-/") 'hippie-expand)
+  (define-key evil-insert-state-map [remap evil-complete-previous] 'hippie-expand)
   )
 
 (defun watcher/local-push-company-backend (backend)
